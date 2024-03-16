9ed01baf89a2502f62d678d92070f0c61aa1bdae
V3lasquez <129894197+V3lasquez@users.noreply.github.com>
2024-03-16 12:44:38 -0500

    Add files via upload
    first commit

---
+++ Lab1/CuentaBancaria.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab1
+{
+    using System;
+    public class CuentaBancaria
+    {
+        public int NumeroCuenta { get; set; }
+        public string Nombre { get; set; }
+        public int Saldo { get; set; }
+        public int Pin { get; set; }
+    }
+
+    public class CajeroAutomatico : CuentaBancaria
+    {
+        public double MostrarSaldo()
+        {
+            return Saldo;
+        }
+
+        public double DepositarFondos(int cantidad)
+        {
+            Saldo += cantidad;
+            return Saldo;
+        }
+
+        public double Retirar(int cantidad)
+        {
+            try
+            {
+                if (cantidad > Saldo)
+                {
+                    Console.WriteLine("Saldo insuficiente para realizar el retiro.");
+                }
+                else
+                {
+                    Saldo -= cantidad;
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Se produjo un error: " + e.Message);
+            }
+            return Saldo;
+        }
+
+        public bool IngresarPin(int numero)
+        {
+            if (numero == Pin)
+            {
+                Console.WriteLine("PIN correcto. Acceso concedido.");
+                return true;
+            }
+            else
+            {
+                Console.WriteLine("PIN incorrecto. Acceso denegado.");
+                return false;
+            }
+        }
+
+        public bool CambiarPin(int nuevoPin)
+        {
+            if (nuevoPin != Pin)
+            {
+                Pin = nuevoPin;
+                Console.WriteLine("PIN cambiado exitosamente.");
+                return true;
+            }
+            else
+            {
+                Console.WriteLine("El pin actual debe ser distinto al anterior.");
+                return false;
+            }
+        }
+    }
+
+
+}
---
+++ Lab1/Program.cs
@@ -0,0 +1,58 @@
+﻿using Lab1;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        CajeroAutomatico cajero = new CajeroAutomatico();
+        cajero.NumeroCuenta = 123456;
+        cajero.Nombre = "Ejemplo Usuario";
+        cajero.Saldo = 1000;
+        cajero.Pin = 1234;
+
+        Console.WriteLine("¡Bienvenido al Cajero Automático!");
+        Console.WriteLine("Por favor, ingrese su PIN:");
+
+        int pinIngresado = int.Parse(Console.ReadLine());
+
+        if (cajero.IngresarPin(pinIngresado))
+        {
+            Console.WriteLine("Acceso concedido.");
+            Console.WriteLine("1. Mostrar saldo");
+            Console.WriteLine("2. Depositar fondos");
+            Console.WriteLine("3. Retirar fondos");
+            Console.WriteLine("4. Cambiar PIN");
+
+            int opcion = int.Parse(Console.ReadLine());
+
+            switch (opcion)
+            {
+                case 1:
+                    Console.WriteLine("Saldo actual: " + cajero.MostrarSaldo());
+                    break;
+                case 2:
+                    Console.Write("Ingrese la cantidad a depositar: ");
+                    int cantidadDeposito = int.Parse(Console.ReadLine());
+                    Console.WriteLine("Nuevo saldo: " + cajero.DepositarFondos(cantidadDeposito));
+                    break;
+                case 3:
+                    Console.Write("Ingrese la cantidad a retirar: ");
+                    int cantidadRetiro = int.Parse(Console.ReadLine());
+                    Console.WriteLine("Nuevo saldo: " + cajero.Retirar(cantidadRetiro));
+                    break;
+                case 4:
+                    Console.Write("Ingrese su nuevo PIN: ");
+                    int nuevoPin = int.Parse(Console.ReadLine());
+                    cajero.CambiarPin(nuevoPin);
+                    break;
+                default:
+                    Console.WriteLine("Opción inválida.");
+                    break;
+            }
+        }
+        else
+        {
+            Console.WriteLine("Acceso denegado.");
+        }
+    }
+}
